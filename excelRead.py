f7541f8d4d60be5a06496b6f67009f224976d1ca
shefreyn <42711053+shefreyn@users.noreply.github.com>
2021-03-18 23:48:27 +0530

    Update excelRead.py

--- excelRead.py
+++ excelRead.py
@@ -1,4 +1,25 @@
-import pylightxl as xl                                     #importing python package to read excel file (.xlsx and .xlsm ) & write (.xlsx)
-print("Hello, welcome to XL Read")                         #test
-db = xl.readxl(fn='C:\Users\Shefreyn\Desktop\Python Excel\excel\test.xlsx')
-print(db)
+#importing python package to read excel file (.xlsx and .xlsm ) & write onto (.xlsx)
+import pylightxl as xl      
+#importing mysql Package
+import mysql.connector
+
+
+
+#TITLE
+print("Hello, welcome to XL Read") 
+                       
+#Reading file & storing data to a var
+xcelFile = xl.readxl(fn='C:/Users/Shefreyn/Desktop/py_excel/excel/test.xlsx')
+
+#printing vals
+print(xcelFile.ws(ws='Sheet1').address(address='A2'),"with ID",xcelFile.ws(ws='Sheet1').address(address='B2')) 
+print(xcelFile.ws(ws='Sheet1').address(address='A3'),"with ID",xcelFile.ws(ws='Sheet1').address(address='B3'))
+print(xcelFile.ws(ws='Sheet1').address(address='A4'),"with ID",xcelFile.ws(ws='Sheet1').address(address='B4'))
+
+#Database connector
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="pma",
+    password="123456"
+)
+print(dataBase)
