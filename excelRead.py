c945c52e93e23899decd11537e2f84e48b78fe6c
shefreyn <42711053+shefreyn@users.noreply.github.com>
2021-03-20 15:52:45 +0530

    Add files via upload

--- excelRead.py
+++ excelRead.py
@@ -3,6 +3,7 @@ import pylightxl as xl
 #importing mysql Package
 import mysql.connector
 
+from mysqlConnector import *
 
 
 #TITLE
@@ -11,16 +12,9 @@ print("Hello, welcome to XL Read")
 #Reading file & storing data to a var
 xcelFile = xl.readxl(fn='C:/Users/Shefreyn/Desktop/py_excel/excel/test.xlsx')
 
-#
 
-#Database connector
-dataBase = mysql.connector.connect(
-    host="localhost",
-    database="excelread_py",
-    user="pma",
-    password="123456"
-)
-print(dataBase)
+
+
 
 #interation
 for row in xcelFile.ws(ws='Sheet1').rows:
@@ -42,9 +36,6 @@ id1 = xcelFile.ws(ws='Sheet1').address(address='B2')
 id2 = xcelFile.ws(ws='Sheet1').address(address='B3')
 id3 = xcelFile.ws(ws='Sheet1').address(address='B4')
 
-#testing
-for i in range(1,3):
-    nameFor1 = xcelFile.ws(ws='Sheet1').address(address='A2')
 
 
 
